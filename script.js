4c7af8c1a818e6f2732f2009b62bbcfda1fe427b
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-16 16:42:12 -0300

    add style

--- script.js
+++ script.js
@@ -7,51 +7,88 @@ const btns_cadastro_clientes = document.getElementById("btns_cadastro_clientes")
 const btns_cadastro_produtos = document.getElementById("btns_cadastro_produtos");
 const btns_cadastro_vendas = document.getElementById("btns_cadastro_vendas");
 
+const btns_mostrar_clientes = document.getElementById("btns_mostrar_clientes");
+const btns_mostrar_produtos = document.getElementById("btns_mostrar_produtos");
+const btns_mostrar_vendas = document.getElementById("btns_mostrar_vendas");
+
 btnHome.addEventListener("click", () => {
     document.getElementById("btns_cadastro").style.display = "none";
     document.getElementById("btns_mostrar").style.display = "none";
     document.getElementById("btns_editar").style.display = "none";
-    document.getElementById("display").style.display = "none";
+    document.getElementById("cadastro_clientes").style.display = "none";
+    document.getElementById("cadastro_produtos").style.display = "none";
+    document.getElementById("cadastro_vendas").style.display = "none";
+    document.getElementById("mostrar_clientes").style.display = "none";
+    document.getElementById("mostrar_produtos").style.display = "none";
+    document.getElementById("mostrar_vendas").style.display = "none";
+ 
 });
 
 btnCadastro.addEventListener("click", () => {
     document.getElementById("btns_cadastro").style.display = "block";
     document.getElementById("btns_mostrar").style.display = "none";
     document.getElementById("btns_editar").style.display = "none";
-    document.getElementById("display").style.display = "none";
+    document.getElementById("cadastros").style.display = "block";
+    document.getElementById("tela_mostrar").style.display = "none";
+ 
 });
 
 btnMostrar.addEventListener("click", () => {
     document.getElementById("btns_cadastro").style.display = "none";
     document.getElementById("btns_mostrar").style.display = "block";
     document.getElementById("btns_editar").style.display = "none";
-    document.getElementById("display").style.display = "none";
+    document.getElementById("cadastro_clientes").style.display = "none";
+    document.getElementById("cadastro_produtos").style.display = "none";
+    document.getElementById("cadastro_vendas").style.display = "none";
+    document.getElementById("tela_mostrar").style.display = "block";
+ 
 });
 
 btnEditar.addEventListener("click", () => {
     document.getElementById("btns_cadastro").style.display = "none";
     document.getElementById("btns_mostrar").style.display = "none";
     document.getElementById("btns_editar").style.display = "block";
-    document.getElementById("display").style.display = "none";
+   ;
 });
 
 btns_cadastro_clientes.addEventListener("click", () => {
     document.getElementById("cadastro_clientes").style.display = "block";
     document.getElementById("cadastro_produtos").style.display = "none";
     document.getElementById("cadastro_vendas").style.display = "none";
-    document.getElementById("display").style.display = "block";
+    
 });
 
 btns_cadastro_produtos.addEventListener("click", () => {
     document.getElementById("cadastro_clientes").style.display = "none";
     document.getElementById("cadastro_produtos").style.display = "block";
     document.getElementById("cadastro_vendas").style.display = "none";
-    document.getElementById("display").style.display = "block";
+   
 });
 
 btns_cadastro_vendas.addEventListener("click", () => {
     document.getElementById("cadastro_clientes").style.display = "none";
     document.getElementById("cadastro_produtos").style.display = "none";
     document.getElementById("cadastro_vendas").style.display = "block";
-    document.getElementById("display").style.display = "block";
+    
+});
+
+btns_mostrar_clientes.addEventListener("click", () => {
+    document.getElementById("mostrar_clientes").style.display = "block";
+    document.getElementById("mostrar_produtos").style.display = "none";
+    document.getElementById("mostrar_vendas").style.display = "none";
+    document.getElementById("cadastros").style.display = "none";
+});
+
+btns_mostrar_produtos.addEventListener("click", () => {
+    document.getElementById("mostrar_clientes").style.display = "none";
+    document.getElementById("mostrar_produtos").style.display = "block";
+    document.getElementById("mostrar_vendas").style.display = "none";
+    document.getElementById("cadastros").style.display = "none";
+});
+
+btns_mostrar_vendas.addEventListener("click", () => {
+    document.getElementById("mostrar_clientes").style.display = "none";
+    document.getElementById("mostrar_produtos").style.display = "none";
+    document.getElementById("mostrar_vendas").style.display = "block";
+    document.getElementById("cadastros").style.display = "none";
 });
