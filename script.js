762a8be222410d7210a1c3c8421b6ef55d1a0716
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-17 21:40:26 -0300

    atualizanco codigo do banco

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
 //ultilizar o indexdb
-const db = window.indexedDB.open("cadastros", 1);
+let db
 
-db.onupgradeneeded = function() {
-    const db = event.target.result;
+const request =indexedDB.open("banco de dados", 1);
+
+request.onerror =(event) => {
+    console.error("erro ao abrir o banco de dados", event.target.error);
+}
+
+request.onupgradeneeded = (event) => {
+    db = event.target.result;
     db.createObjectStore("clientes", {keyPath: "id", autoIncrement: true});
     db.createObjectStore("produtos", {keyPath: "id", autoIncrement: true});
     db.createObjectStore("vendas", {keyPath: "id", autoIncrement: true});
-};  
+}
 
-const cadastros = db.transaction(["clientes", "produtos", "vendas"], "readwrite");
-const clientes = cadastros.objectStore("clientes");
-const produtos = cadastros.objectStore("produtos");
-const vendas = cadastros.objectStore("vendas");
-
-const btnCadastrarClientes = document.getElementById("cadastrar_clientes");
-const btnCadastrarProdutos = document.getElementById("cadastrar_produtos");
-const btnCadastrarVendas = document.getElementById("cadastrar_vendas");
-
-btnCadastrarClientes.addEventListener("click", () => {
-    const nome = document.getElementById("nome_clientes").value;
-    cadastros.objectStore("clientes").add({nome});
-    document.getElementById("nome_clientes").value = "";
-});
-
-btnCadastrarProdutos.addEventListener("click", () => {
-    const nome = document.getElementById("nome_produtos").value;
-    const preco = document.getElementById("preco_produtos").value;
-    const quantidade = document.getElementById("quantidade_produtos").value;
-    cadastros.objectStore("produtos").add({nome, preco, quantidade});
-    document.getElementById("nome_produtos").value = "";
-    document.getElementById("preco_produtos").value = "";
-    document.getElementById("quantidade_produtos").value = "";
-});
-
-btnCadastrarVendas.addEventListener("click", () => {
-    const nome_cliente = document.getElementById("nome_vendas").value;
-    const nome_produto = document.getElementById("nome_produtos_vendas").value;
-    const quantidade = document.getElementById("quantidade_vendas").value;
-    const preco = document.getElementById("preco_vendas").value;
-    cadastros.objectStore("vendas").add({nome_cliente, nome_produto, quantidade, preco});
-    document.getElementById("nome_vendas").value = "";
-    document.getElementById("nome_produtos_vendas").value = "";
-    document.getElementById("quantidade_vendas").value = "";
-    document.getElementById("preco_vendas").value = "";
-});
-
+request.onsuccess = (event) => {
+    db = event.target.result;
+    console.log("banco de dados aberto com sucesso", db);
+}
 
 
 
@@ -160,3 +133,49 @@ btns_editar_produtos.addEventListener("click", () => {
     document.getElementById("editar_clientes").style.display = "none";
     document.getElementById("editar_produtos").style.display = "block";
 });
+
+db.onupgradeneeded = function() {
+    const db = event.target.result;
+    db.createObjectStore("clientes", {keyPath: "id", autoIncrement: true});
+    db.createObjectStore("produtos", {keyPath: "id", autoIncrement: true});
+    db.createObjectStore("vendas", {keyPath: "id", autoIncrement: true});
+};  
+
+const cadastros = db.transaction(["clientes", "produtos", "vendas"], "readwrite");
+const clientes = cadastros.objectStore("clientes");
+const produtos = cadastros.objectStore("produtos");
+const vendas = cadastros.objectStore("vendas");
+
+const btnCadastrarClientes = document.getElementById("cadastrar_clientes");
+const btnCadastrarProdutos = document.getElementById("cadastrar_produtos");
+const btnCadastrarVendas = document.getElementById("cadastrar_vendas");
+
+btnCadastrarClientes.addEventListener("click", () => {
+    const nome = document.getElementById("nome_clientes").value;
+    cadastros.objectStore("clientes").add({nome});
+    document.getElementById("nome_clientes").value = "";
+});
+
+btnCadastrarProdutos.addEventListener("click", () => {
+    const nome = document.getElementById("nome_produtos").value;
+    const preco = document.getElementById("preco_produtos").value;
+    const quantidade = document.getElementById("quantidade_produtos").value;
+    cadastros.objectStore("produtos").add({nome, preco, quantidade});
+    document.getElementById("nome_produtos").value = "";
+    document.getElementById("preco_produtos").value = "";
+    document.getElementById("quantidade_produtos").value = "";
+});
+
+btnCadastrarVendas.addEventListener("click", () => {
+    const nome_cliente = document.getElementById("nome_vendas").value;
+    const nome_produto = document.getElementById("nome_produtos_vendas").value;
+    const quantidade = document.getElementById("quantidade_vendas").value;
+    const preco = document.getElementById("preco_vendas").value;
+    cadastros.objectStore("vendas").add({nome_cliente, nome_produto, quantidade, preco});
+    document.getElementById("nome_vendas").value = "";
+    document.getElementById("nome_produtos_vendas").value = "";
+    document.getElementById("quantidade_vendas").value = "";
+    document.getElementById("preco_vendas").value = "";
+});
+
+console.log(cadastros, clientes, produtos, vendas);
