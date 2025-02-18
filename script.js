1c617eed410e99005ba9df95c13953aa36286592
Ricardo-10 <ricardomjr2@gmail.com>
2025-02-17 21:31:27 -0300

    create func cadastrar

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+//ultilizar o indexdb
+const db = window.indexedDB.open("cadastros", 1);
+
+db.onupgradeneeded = function() {
+    const db = event.target.result;
+    db.createObjectStore("clientes", {keyPath: "id", autoIncrement: true});
+    db.createObjectStore("produtos", {keyPath: "id", autoIncrement: true});
+    db.createObjectStore("vendas", {keyPath: "id", autoIncrement: true});
+};  
+
+const cadastros = db.transaction(["clientes", "produtos", "vendas"], "readwrite");
+const clientes = cadastros.objectStore("clientes");
+const produtos = cadastros.objectStore("produtos");
+const vendas = cadastros.objectStore("vendas");
+
+const btnCadastrarClientes = document.getElementById("cadastrar_clientes");
+const btnCadastrarProdutos = document.getElementById("cadastrar_produtos");
+const btnCadastrarVendas = document.getElementById("cadastrar_vendas");
+
+btnCadastrarClientes.addEventListener("click", () => {
+    const nome = document.getElementById("nome_clientes").value;
+    cadastros.objectStore("clientes").add({nome});
+    document.getElementById("nome_clientes").value = "";
+});
+
+btnCadastrarProdutos.addEventListener("click", () => {
+    const nome = document.getElementById("nome_produtos").value;
+    const preco = document.getElementById("preco_produtos").value;
+    const quantidade = document.getElementById("quantidade_produtos").value;
+    cadastros.objectStore("produtos").add({nome, preco, quantidade});
+    document.getElementById("nome_produtos").value = "";
+    document.getElementById("preco_produtos").value = "";
+    document.getElementById("quantidade_produtos").value = "";
+});
+
+btnCadastrarVendas.addEventListener("click", () => {
+    const nome_cliente = document.getElementById("nome_vendas").value;
+    const nome_produto = document.getElementById("nome_produtos_vendas").value;
+    const quantidade = document.getElementById("quantidade_vendas").value;
+    const preco = document.getElementById("preco_vendas").value;
+    cadastros.objectStore("vendas").add({nome_cliente, nome_produto, quantidade, preco});
+    document.getElementById("nome_vendas").value = "";
+    document.getElementById("nome_produtos_vendas").value = "";
+    document.getElementById("quantidade_vendas").value = "";
+    document.getElementById("preco_vendas").value = "";
+});
+
+
+
+
 const btnCadastro = document.getElementById("cadastro");
 const btnMostrar = document.getElementById("mostrar");
 const btnEditar = document.getElementById("editar");
